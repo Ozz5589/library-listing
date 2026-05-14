84c26f1fc01b3a85896a141dd8f4ff962f35a77a
Ozz5589 <oswaldjackson0@gmail.com>
2026-05-14 04:59:23 -0700

    Initial commit with code and tests

---
+++ src/main/resources/archetype-resources/src/main/java/Listing.java
@@ -0,0 +1,34 @@
+public class Listing {
+    private String title;
+    private int quantity;
+    private boolean soldOut;
+
+    public Listing(String title, int quantity) {
+        if (quantity &lt; 1) {
+            throw new IllegalArgumentException("Quantity must be at least 1");
+        }
+        this.title = title;
+        this.quantity = quantity;
+        updateStatus();
+    }
+
+    public void sellOne() {
+        if (soldOut) {
+            throw new IllegalStateException("Listing is sold out");
+        }
+        quantity--;
+        updateStatus();
+    }
+
+    private void updateStatus() {
+        soldOut = (quantity <= 0);
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public boolean isSoldOut() {
+        return soldOut;
+    }
+}
